b007a5b789857a193bcbdca4db5760dd91bf9c62
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2022-12-24 20:45:14 +0900

    firebase setting success

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import firebase from "./firebase";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(firebase);
 root.render(
   <React.StrictMode>
     <App />
